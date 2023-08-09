351fa7f898ff3816c762481e3073f35f0d8ddbca
J.B0203 <gt20181122@gmail.com>
2023-08-09 08:15:02 -0500

    update firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -7,8 +7,7 @@ const firebaseConfig = {
     projectId: process.env.NEXT_PUBLIC_PROJECTID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-    appId: process.env.NEXT_PUBLIC_APPID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
+    appId: process.env.NEXT_PUBLIC_APPID
 };
 
 export const app = initializeApp(firebaseConfig);
